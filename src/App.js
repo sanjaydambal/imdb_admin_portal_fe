1aaed5952c02f6a229d07f51f5ceb9cd983e7026
sanjaydambal <sanjaydambal@gmail.com>
2024-04-19 19:01:03 +0530

    created logic to confirm and delete the movies

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import UpdateMovie from './components/UpdateMovie';
+
 function App() {
     const [movies, setMovies] = useState([]);
     const [updateMovies,setUpdateMovies] = useState(null)
-
+    const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+    const [movieToDelete, setMovieToDelete] = useState(null);
     useEffect(() => {
         fetchMovies();
     }, []);
@@ -28,6 +30,28 @@ function App() {
     const handleCancel = () => {
         setUpdateMovies(null)
     }
+  
+    const handleDeleteMovie = async (id) => {
+        try {
+            setMovieToDelete(id);
+            setShowConfirmationModal(true);
+        } catch (error) {
+            console.error('Error deleting movie:', error);
+        }
+    };
+    
+    
+
+    const confirmDelete = async () => {
+        try {
+            await axios.delete(`http://localhost:4000/api/movies/${movieToDelete}`);
+            await fetchMovies();
+        } catch (error) {
+            console.error('Error deleting movie:', error);
+        }
+        setShowConfirmationModal(false);
+        setMovieToDelete(null);
+    };
 
     return (
         <div className="App">
@@ -43,11 +67,24 @@ function App() {
                                     <p className="card-text">{movie.description}</p>
                                     <p className="card-text"><small className="text-muted">Release Date: {movie.release_date}</small></p>
                                     <p className="card-text"><small className="text-muted">Genre: {movie.genre}</small></p>
+                                    <button className="bg-blue-500 text-white py-2 px-4 rounded-md mt-4 btn-primary primary gap-1" onClick={() => setUpdateMovies(movie)}>Update</button>
+                                    <button className="bg-red-500 text-white py-2 px-4 rounded-md" onClick={() => handleDeleteMovie(movie.id)}>Delete</button>
                                 </div>
                             </div>
                         </div>
                     ))}
                 </div>
+                {showConfirmationModal && (
+                    <div className="modal">
+                        <div className="modal-content">
+                            <p>Are you sure you want to delete this movie?</p>
+                            <div>
+                                <button onClick={confirmDelete}>Confirm</button>
+                                <button onClick={() => setShowConfirmationModal(false)}>Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     );
